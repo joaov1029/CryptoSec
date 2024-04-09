321b8dcf8112b6c5302e62f804e6a9775833b9f6
João Victor Sales Herculano <107000714+joaov1029@users.noreply.github.com>
2024-04-09 13:22:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function SetBr(){
+    let ptBr = document.getElementById("ptBr")
+let enUs = document.getElementById("enUs")
+ptBr.style.display = "block"
+enUs.style.display = "none"
+}
+
+function SetUsa(){
+    let ptBr = document.getElementById("ptBr")
+let enUs = document.getElementById("enUs")
+ptBr.style.display = "none"
+enUs.style.display = "block"
+}
